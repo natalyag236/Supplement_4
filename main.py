483907f325908cf94a0c0ac70134b2c02eb9df41
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-24 18:10:35 -0500

    doc: added documentation to next ten numbers

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 def next_ten_nums(num):
+    """Given a number, returns a string contatinning the next 10 nums spearated by commaas
+
+    Args:
+        num (int): the starting number
+
+    Returns:
+        str: a comma-separated string of the next 10 numbers
+    """
     numbers = []
     for i in range(1,11):
         numbers.append(num + i)
     return ', '.join(map(str, numbers)) 
+
 def test_ten_nums():
     assert next_ten_nums(23) == "24, 25, 26, 27, 28, 29, 30, 31, 32, 33" 
