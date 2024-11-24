e439010597635a950389a0303d2daab0b510059c
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-24 18:53:58 -0500

    doc: added documentation to comma delimited string

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ def test_ten_nums():
     assert next_ten_nums(23) == "24, 25, 26, 27, 28, 29, 30, 31, 32, 33" 
 
 def comma_delimited_string(string_list):
+    """Converts a list of strings into a single comma delimited string and 
+    separates by a comma 
+
+    Args:
+        string_list (list): a list of string to be joined
+
+    Returns:
+        str: a single string with the list elements separated by commas.
+    """
     return ', '.join(string_list) 
 
 def test_comma_string():
