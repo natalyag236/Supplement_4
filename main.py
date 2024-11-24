d3b8dad0d1a3771b72c79ebc622ed1e7ff444d65
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-24 18:06:43 -0500

    feat: implemented function to next ten nums function to generate next 10 nums after a given number

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+def next_ten_nums(num):
+    numbers = []
+    for i in range(1,11):
+        numbers.append(num + i)
+    return ', '.join(map(str, numbers)) 
 def test_ten_nums():
     assert next_ten_nums(23) == "24, 25, 26, 27, 28, 29, 30, 31, 32, 33" 
